b9ebae2e2ae47a55d0eabd160f4b27190b7cc7a2
Edgar Zambrana <edgarjoel0920@gmail.com>
2025-06-20 12:28:28 -0400

    Update basic task service file to reach into the repository for the 'data'

--- src/main/java/com/example/reactive_backend/service/TaskService.java
+++ src/main/java/com/example/reactive_backend/service/TaskService.java
@@ -1,13 +1,21 @@
 package com.example.reactive_backend.service;
 
+import com.example.reactive_backend.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
+import java.util.Collections;
+
 @Service
-@RequiredArgsConstructor
 public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
     public Flux<String> getAllTasks() {
-        return Flux.just("Hello", "World", "Coming", "From", "The", "Zambrana", "Household");
+        return taskRepository.getAllTasks()
+                .flatMapIterable(str -> Collections.singleton(str + ", "));
     }
 }
