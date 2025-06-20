667e5df9bdd90d88e2420588bd0189704948a22d
Edgar Zambrana <edgarjoel0920@gmail.com>
2025-06-20 00:14:52 -0400

    Initial commit -m

---
+++ src/main/java/com/example/reactive_backend/ReactiveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactive_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reactive_backend/ReactiveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactive_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
