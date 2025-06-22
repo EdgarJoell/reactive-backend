60a94db880b2f592f4b9906617c0ba85fb561f7f
Edgar Zambrana <edgarjoel0920@gmail.com>
2025-06-22 17:43:11 -0400

    Update Exception classes to include their intended response status codes.

--- src/main/java/com/example/reactive_backend/errorhandling/exception/CouldNotInsertException.java
+++ src/main/java/com/example/reactive_backend/errorhandling/exception/CouldNotInsertException.java
@@ -1,7 +1,9 @@
-package com.example.reactive_backend.exception;
+package com.example.reactive_backend.errorhandling.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class CouldNotInsertException extends RuntimeException {
     public CouldNotInsertException(String message, HttpStatus httpStatus) {
         super(message);
