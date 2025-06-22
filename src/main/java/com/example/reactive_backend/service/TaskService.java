6f0950fa7c52e6d24aafc375cd61b977742e0dda
Edgar Zambrana <edgarjoel0920@gmail.com>
2025-06-21 21:48:05 -0400

    Update TaskService file to follow enterprise standards and make methods retrieve data from DB instead of practice data

--- src/main/java/com/example/reactive_backend/service/TaskService.java
+++ src/main/java/com/example/reactive_backend/service/TaskService.java
@@ -1,21 +1,22 @@
 package com.example.reactive_backend.service;
 
+import com.example.reactive_backend.model.Task;
 import com.example.reactive_backend.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
-
-import java.util.Collections;
+import reactor.core.publisher.Mono;
 
 @Service
+@RequiredArgsConstructor
 public class TaskService {
     private final TaskRepository taskRepository;
 
-    public TaskService(TaskRepository taskRepository) {
-        this.taskRepository = taskRepository;
+    public Flux<Task> getAllTasks() {
+        return taskRepository.getAllTasks();
     }
-    public Flux<String> getAllTasks() {
-        return taskRepository.getAllTasks()
-                .flatMapIterable(str -> Collections.singleton(str + ", "));
+
+    public Mono<Task> createOneTask(Task task) {
+        return taskRepository.createTask(task);
     }
 }
