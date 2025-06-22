978716cba18242c69730da219bcce4922447151e
Edgar Zambrana <edgarjoel0920@gmail.com>
2025-06-21 21:47:01 -0400

    Update TaskController to follow enterprise standards with lombok usage and update endpoints to retrieve data from DB instead of practice methods.

--- src/main/java/com/example/reactive_backend/controller/TaskController.java
+++ src/main/java/com/example/reactive_backend/controller/TaskController.java
@@ -1,22 +1,25 @@
 package com.example.reactive_backend.controller;
 
+import com.example.reactive_backend.model.Task;
 import com.example.reactive_backend.service.TaskService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping(path = "/api")
+@RequiredArgsConstructor
 public class TaskController {
     private final TaskService taskService;
 
-    public TaskController(TaskService taskService) {
-        this.taskService = taskService;
+    @GetMapping(value = "/tasks")
+    public Flux<Task> getAllTasks() {
+        return taskService.getAllTasks();
     }
 
-    @GetMapping(value = "/tasks")
-    public Flux<String> getAllTasks() {
-        return taskService.getAllTasks();
+    @PostMapping(value = "/task")
+    public Mono<Task> createOneTask(@RequestBody Task task) {
+        return taskService.createOneTask(task);
     }
 }
