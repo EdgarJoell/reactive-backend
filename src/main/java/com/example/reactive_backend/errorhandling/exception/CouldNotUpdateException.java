afb1e19e0aa35ad8be62aeadcc129e31d4177e92
Edgar Zambrana <edgarjoel0920@gmail.com>
2025-06-22 22:30:05 -0400

    Create CouldNotUpdateException class

---
+++ src/main/java/com/example/reactive_backend/errorhandling/exception/CouldNotUpdateException.java
@@ -0,0 +1,12 @@
+package com.example.reactive_backend.errorhandling.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class CouldNotUpdateException extends RuntimeException {
+
+    public CouldNotUpdateException(String message) {
+        super(message);
+    }
+}
