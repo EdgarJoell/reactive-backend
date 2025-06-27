eca77e7b9c96b0359609f2a0f1864ae05d34e46d
Edgar Zambrana <edgarjoel0920@gmail.com>
2025-06-27 02:00:42 -0400

    Update CouldNotInsertException to remove HttpStatus parameter

--- src/main/java/com/example/reactive_backend/errorhandling/exception/CouldNotInsertException.java
+++ src/main/java/com/example/reactive_backend/errorhandling/exception/CouldNotInsertException.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class CouldNotInsertException extends RuntimeException {
-    public CouldNotInsertException(String message, HttpStatus httpStatus) {
+    public CouldNotInsertException(String message) {
         super(message);
     }
 }
