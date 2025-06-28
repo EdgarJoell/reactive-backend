c2101a7da560f990f64393424541233970b13bc7
Edgar Zambrana <edgarjoel0920@gmail.com>
2025-06-28 15:21:26 -0400

    Write integration tests for getOneTask endpoint

--- src/testintegration/java/com/example/reactive_backend/task/TaskControllerIntegrationTest.java
+++ src/testintegration/java/com/example/reactive_backend/task/TaskControllerIntegrationTest.java
@@ -4,14 +4,22 @@ import com.example.reactive_backend.IntegrationTestConfig;
 import com.example.reactive_backend.ReactiveBackendIntegrationTest;
 import com.example.reactive_backend.controller.TaskController;
 import com.example.reactive_backend.errorhandling.ErrorAdviceDto;
+import com.example.reactive_backend.errorhandling.exception.NotFoundException;
 import com.example.reactive_backend.model.Task;
 import com.example.reactive_backend.service.TaskService;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.mongodb.core.FindAndModifyOptions;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.reactive.function.BodyInserters;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -23,6 +31,8 @@ public class TaskControllerIntegrationTest extends ReactiveBackendIntegrationTes
 
     @Autowired
     private TaskService service;
+
+    private final String id = "685724022e21a9baae11f00f";
 
     @Test
     void testGetAllTasksEndpoint() {
@@ -55,6 +65,71 @@ public class TaskControllerIntegrationTest extends ReactiveBackendIntegrationTes
                     assertThat(actual.getResponseBody().getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
                     assertThat(actual.getResponseBody().getPath()).isEqualTo("/api/tasks");
                     assertThat(actual.getResponseBody().getMessage()).isNotEmpty();
+                });
+    }
+
+    @Test
+    void testGetOneTaskEndpoint() {
+        Task task = Task.builder().id(new ObjectId(id)).title("Integration Test Task").description("The description for the Document being returned.").completed(true).build();
+
+        when(mongoTemplate.findById(new ObjectId(id), Task.class)).thenReturn(Mono.just(task));
+        when(service.getOneTask(new ObjectId(id))).thenReturn(Mono.just(task));
+
+        webTestClient.get()
+                .uri("/api/task?id=%s".formatted(id))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Task.class)
+                .isEqualTo(task);
+    }
+
+    @Test
+    void testGetOneTaskEndpointBadRequestException() {
+        webTestClient.get()
+                .uri("/api/task?id=685724022e2")
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody(ErrorAdviceDto.class)
+                .consumeWith(actual -> {
+                    assertThat(actual.getResponseBody().getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+                    assertThat(actual.getResponseBody().getPath()).isEqualTo("/api/task");
+                    assertThat(actual.getResponseBody().getMessage()).isNotEmpty();
+                    assertThat(actual.getResponseBody().getHttpMethod()).isEqualTo(HttpMethod.GET.name());
+                });
+    }
+
+    @Test
+    void testGetOneTaskEndpointNotFoundException() {
+        when(mongoTemplate.findById(new ObjectId(id), Task.class)).thenReturn(Mono.empty());
+        when(service.getOneTask(new ObjectId(id))).thenReturn(Mono.error(new NotFoundException("Document not found bro")));
+
+        webTestClient.get()
+                .uri("/api/task?id=%s".formatted(id))
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody(ErrorAdviceDto.class)
+                .consumeWith(actual -> {
+                    assertThat(actual.getResponseBody().getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
+                    assertThat(actual.getResponseBody().getPath()).isEqualTo("/api/task");
+                    assertThat(actual.getResponseBody().getMessage()).isNotEmpty();
+                    assertThat(actual.getResponseBody().getHttpMethod()).isEqualTo(HttpMethod.GET.name());
+                });
+    }
+
+    @Test
+    void testGetOneTaskEndpointRuntimeException() {
+        when(service.getOneTask(new ObjectId(id))).thenReturn(Mono.error(new RuntimeException("Runtime exception.")));
+
+        webTestClient.get()
+                .uri("/api/task?id=%s".formatted(id))
+                .exchange()
+                .expectStatus().is5xxServerError()
+                .expectBody(ErrorAdviceDto.class)
+                .consumeWith(actual -> {
+                    assertThat(actual.getResponseBody().getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+                    assertThat(actual.getResponseBody().getPath()).isEqualTo("/api/task");
+                    assertThat(actual.getResponseBody().getMessage()).isNotEmpty();
+                    assertThat(actual.getResponseBody().getHttpMethod()).isEqualTo(HttpMethod.GET.name());
                 });
     }
 }
